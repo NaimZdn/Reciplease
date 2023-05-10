c36ead439c61368ac7c8ce60a7ba2b305871b4a1
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-10 18:23:15 +0200

    Initial Commit

---
+++ Reciplease/RecipleaseApp.swift
@@ -0,0 +1,20 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by Zidouni on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
