92b69e7410476199306d113e1bc68a2fee24a826
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-11 21:34:17 +0200

    feat(global) : create optionButton component

---
+++ Reciplease/Presentation/Components/OptionButton.swift
@@ -0,0 +1,35 @@
+//
+//  OptionButton.swift
+//  Reciplease
+//
+//  Created by Zidouni on 11/05/2023.
+//
+
+import SwiftUI
+
+struct OptionButton: View {
+    var icon: String
+    
+    var body: some View {
+        
+        Button {
+            
+        } label: {
+            Image(icon)
+                .foregroundColor(.primaryColor)
+                .frame(maxWidth: 37, maxHeight: 37)
+                .background(in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.primaryColor, lineWidth: 2 )
+                )
+        }
+        .buttonStyle(PlainButtonStyle())    
+    }
+}
+
+struct OptionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionButton(icon: "filter")
+    }
+}
