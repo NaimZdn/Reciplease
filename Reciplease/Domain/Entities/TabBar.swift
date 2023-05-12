89d9199a589845d18cd3817179f0514508ae166a
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-12 15:14:31 +0200

    feat(tabBar) : add TabBar Model to control each view

---
+++ Reciplease/Domain/Entities/TabBar.swift
@@ -0,0 +1,14 @@
+//
+//  TabBar.swift
+//  Reciplease
+//
+//  Created by Zidouni on 12/05/2023.
+//
+
+import Foundation
+
+enum TabBar: String, CaseIterable {
+    case cart
+    case favorite
+    case seetings
+}
