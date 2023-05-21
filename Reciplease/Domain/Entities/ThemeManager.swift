22fc9ce04df0aded08bc41ddc3b266de50cc6fac
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-21 15:14:54 +0200

    feat(theme) : add ThemeManager class to control the app color mode

---
+++ Reciplease/Domain/Entities/ThemeManager.swift
@@ -0,0 +1,22 @@
+//
+//  ThemeManager.swift
+//  Reciplease
+//
+//  Created by Zidouni on 10/05/2023.
+//
+
+import Foundation
+import UIKit
+
+class ThemeManager {
+    
+    static let shared = ThemeManager()
+    
+    private init() {}
+    
+    func handleTheme(darkMode: Bool) {
+        
+        UIApplication.shared.windows.first?.overrideUserInterfaceStyle = darkMode ? .dark : .light
+    
+    }
+}
