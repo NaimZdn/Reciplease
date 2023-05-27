162f01569d339eea2646f49c3f76f0faec362711
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-27 15:00:23 +0200

    feat(favorite) : add header of FavoriteView

---
+++ Reciplease/Presentation/Favorite/View/FavoriteView.swift
@@ -0,0 +1,56 @@
+//
+//  FavoriteView.swift
+//  Reciplease
+//
+//  Created by Zidouni on 27/05/2023.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    @State private var searchText = ""
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("Favorite")
+                    .font(.defaultTitle)
+                    .foregroundColor(Color.primaryColor)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                HStack(spacing: 15) {
+                    TextField("Search for a recipe", text: $searchText)
+                        .font(.defaultPlaceholder)
+                        .frame(maxWidth: 240)
+                        .overlay(
+                            Rectangle()
+                                .frame(height: 2)
+                                .foregroundColor(.primaryColor)
+                                .padding(.top, 35)
+                        )
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    OptionButton(icon: "filter")
+                    
+                }
+            }
+            .padding(.vertical, 10)
+            .background(Color.background)
+            
+            ScrollView {
+                
+                
+            }
+       
+        }
+        .padding(.horizontal, 20)
+        .background(Color.background)
+        
+    }
+}
+
+struct FavoriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteView()
+    }
+}
