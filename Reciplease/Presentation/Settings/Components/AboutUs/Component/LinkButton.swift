69456855e2ef180d165be719589bd815f1697af9
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-11 21:39:37 +0200

    feat(settings) : add LinkButton component for the AboutUsView

---
+++ Reciplease/Presentation/Settings/Components/AboutUs/Component/LinkButton.swift
@@ -0,0 +1,34 @@
+//
+//  LinkButton.swift
+//  Reciplease
+//
+//  Created by Zidouni on 11/06/2023.
+//
+
+import SwiftUI
+
+struct LinkButton: View {
+    @Binding var isPresented: Bool
+    var icon: String
+    var link: String
+    
+    var body: some View {
+        Button {
+            self.isPresented.toggle()
+        } label: {
+            Image(icon)
+                .resizable()
+                .foregroundColor(.primaryColor)
+                .frame(width: 30, height: 30)
+        }
+        .sheet(isPresented: $isPresented) {
+            Safari(url: link)
+        }
+    }
+}
+
+struct LinkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LinkButton(isPresented: .constant(false), icon: "linkedin", link: "https://www.linkedin.com/in/naim-zidouni/")
+    }
+}
