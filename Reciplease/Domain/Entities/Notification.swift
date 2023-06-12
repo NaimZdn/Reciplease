7bfa63a516c7bd8aec4bf1eb588eaf68fe479224
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-12 22:02:48 +0200

    feat(model) : create a Notification struct

---
+++ Reciplease/Domain/Entities/Notification.swift
@@ -0,0 +1,17 @@
+//
+//  Notification.swift
+//  Reciplease
+//
+//  Created by Zidouni on 12/06/2023.
+//
+
+import Foundation
+
+struct Notification {
+    let identifier: String
+    let title: String
+    let body: String
+    let hour: Int
+    let minutes: Int
+    let isDaily: Bool
+}
