db158fe98baec0347164550e07caef5065240de1
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-16 19:30:00 +0200

    feat(cart) : add ternary operator to check if the ingredient is an emoji or not + disabled the button label if ingredientsSelected is is greater than or equal to 9

--- Reciplease/Presentation/Cart/Component/IngredientLabel.swift
+++ Reciplease/Presentation/Cart/Component/IngredientLabel.swift
@@ -8,19 +8,32 @@
 import SwiftUI
 
 struct IngredientLabel: View {
+    @Binding var ingredientsSelected: [Ingredient]
     @Binding var isSelected: Bool
     var ingredientIcon: String
     var ingredientName: String
     
+    
+    @ObservedObject private var viewModel = CartViewModel()
+    
     var body: some View {
         VStack {
-            
             Button {
                 isSelected.toggle()
             } label: {
                 VStack(spacing: 10) {
                     Text(ingredientIcon)
-                        .font(.defaultTitle)
+                        .font( viewModel.isNotEmoji(ingredientIcon) ? .defaultTitle2 : .defaultTitle)
+                        .background(
+                            viewModel.isNotEmoji(ingredientIcon) ? Color.addIngredientIcon
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .mask(
+                                    Capsule()
+                                        .fill()
+                                        .frame(width: 30, height: 30, alignment: .topLeading)
+                                ) : nil
+                        )
+                        .padding(.vertical,  viewModel.isNotEmoji(ingredientIcon) ? 5 : 0 )
                     
                     Text(ingredientName)
                         .font(.defaultBody)
@@ -34,6 +47,7 @@ struct IngredientLabel: View {
                         .stroke(Color.selectorStroke, lineWidth: isSelected ? 1 : 0)
                 )
             }
+            .disabled(ingredientsSelected.count >= 9 && !isSelected)
             .buttonStyle(PlainButtonStyle())
         }
     }
@@ -44,6 +58,6 @@ struct IngredientLabel_Previews: PreviewProvider {
     @ObservedObject static private var viewModel = CartViewModel()
     
     static var previews: some View {
-        IngredientLabel(isSelected: .constant(true), ingredientIcon: viewModel.ingredients[1].icon, ingredientName: viewModel.ingredients[1].name)
+        IngredientLabel(ingredientsSelected: .constant([]), isSelected: .constant(false), ingredientIcon: viewModel.ingredients[1].icon, ingredientName: viewModel.ingredients[1].name)
     }
 }
