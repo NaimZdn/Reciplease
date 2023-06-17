554d21b9a9be5c34a9d02a5ca3e5a762352aba8a
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-17 19:30:11 +0200

    feat(global) : add some properties to the CustomTextField component

--- Reciplease/Presentation/Components/CustomTextField.swift
+++ Reciplease/Presentation/Components/CustomTextField.swift
@@ -8,26 +8,52 @@
 import SwiftUI
 
 struct CustomTextField: View {
+    @ObservedObject private var viewModel = CartViewModel()
+    
     @Binding var text: String
-    var size: CGFloat
     var placeholder: String
+    var charactersLimit: Int
+    var size: CGFloat
+    var charactersCounter: Bool
     
     var body: some View {
+        
         VStack(alignment: .trailing, spacing: 15) {
-            TextField(placeholder, text: $text)
-                .font(.defaultPlaceholder)
-                .overlay(
-                    Rectangle()
-                        .frame(height: 2)
-                        .foregroundColor(.primaryColor)
-                        .padding(.top, 35)
-                )
-                .frame(maxWidth: .infinity, alignment: .leading)
+            ZStack(alignment: .trailing) {
+                TextField(placeholder, text: $text)
+                    .font(.defaultPlaceholder)
+                    .overlay(
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.primaryColor)
+                            .padding(.top, 35)
+                    )
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .onChange(of: text) { character in
+                        if character.count > charactersLimit {
+                            text.removeLast()
+                        }
+                        if viewModel.containsOnlySpaces(text) {
+                            text = ""
+                        }
+                    }
+                Button {
+                    text = ""
+                } label: {
+                    if !text.isEmpty {
+                        Image("xmark")
+                            .foregroundColor(Color.primaryColor)
+                            .padding(.trailing, 7)
+                    }
+                }
+            }
             
-            Text("0/1")
-                .font(.defaultLabelCaption)
-                .foregroundColor(Color.primaryColor)
-            
+            if charactersCounter {
+                Text("\(text.count)/\(charactersLimit)")
+                    .font(.defaultLabelCaption)
+                    .foregroundColor(Color.primaryColor)
+                
+            }
         }
         .frame(maxWidth: size)
     }
@@ -35,6 +61,6 @@ struct CustomTextField: View {
 
 struct CustomTextField_Previews: PreviewProvider {
     static var previews: some View {
-        CustomTextField(text: .constant(""), size: 60, placeholder: "ID")
+        CustomTextField(text: .constant(""), placeholder: "ID", charactersLimit: 1, size: 60, charactersCounter: true)
     }
 }
