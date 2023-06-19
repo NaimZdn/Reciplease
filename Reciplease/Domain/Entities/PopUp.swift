66dad9d0be45b618207458ce4bc540c0cacafaa6
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-19 23:13:37 +0200

    feat(global) : create a model PopUp with all message cases

---
+++ Reciplease/Domain/Entities/PopUp.swift
@@ -0,0 +1,16 @@
+//
+//  PopUp.swift
+//  Reciplease
+//
+//  Created by Zidouni on 18/06/2023.
+//
+
+import Foundation
+
+enum Message: String {
+    case addIngredient = "Your ingredient has been created !"
+    case deleteIngredient = "Your ingredient has been deleted !"
+    case deleteCart = "Your cart has been emptied !"
+    case addIngredientAlreadyExist = "This ingredient is already available !"
+    
+}
