3c0f50b87c17d5d32d397e304dbe05f82b802442
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-21 17:05:33 +0200

    feat(network) : add API decodable

---
+++ Reciplease/Data/Network/Data Mapping/Recipe+Decodable.swift
@@ -0,0 +1,113 @@
+//
+//  Recipe+Decodable.swift
+//  Reciplease
+//
+//  Created by Zidouni on 20/06/2023.
+//
+
+import Foundation
+
+// MARK: - Recipe
+struct Recipe: Codable {
+    let from, to, count: Int
+    let links: RecipeLinks
+    let hits: [Hit]
+
+    enum CodingKeys: String, CodingKey {
+        case from, to, count
+        case links = "_links"
+        case hits
+    }
+}
+
+// MARK: - Hit
+struct Hit: Codable {
+    let recipe: RecipeClass
+    let links: HitLinks
+
+    enum CodingKeys: String, CodingKey {
+        case recipe
+        case links = "_links"
+    }
+}
+
+// MARK: - HitLinks
+struct HitLinks: Codable {
+    let linksSelf: Next
+
+    enum CodingKeys: String, CodingKey {
+        case linksSelf = "self"
+    }
+}
+
+// MARK: - Next
+struct Next: Codable {
+    let href: String
+    let title: Title
+}
+
+enum Title: String, Codable {
+    case nextPage = "Next page"
+    case titleSelf = "Self"
+}
+
+// MARK: - RecipeClass
+struct RecipeClass: Codable {
+    let label: String
+    let image: String
+    let ingredients: [IngredientsResponse]
+    let calories: Double
+    let totalTime: Double
+    let digest: [Digest]
+}
+
+// MARK: - Digest
+struct Digest: Codable {
+    let label, tag: String
+    let schemaOrgTag: SchemaOrgTag?
+    let total: Double
+    let hasRDI: Bool
+    let daily: Double
+    let unit: Unit
+    let sub: [Digest]?
+}
+
+enum SchemaOrgTag: String, Codable {
+    case carbohydrateContent = "carbohydrateContent"
+    case cholesterolContent = "cholesterolContent"
+    case fatContent = "fatContent"
+    case fiberContent = "fiberContent"
+    case proteinContent = "proteinContent"
+    case saturatedFatContent = "saturatedFatContent"
+    case sodiumContent = "sodiumContent"
+    case sugarContent = "sugarContent"
+    case transFatContent = "transFatContent"
+}
+
+enum Unit: String, Codable {
+    case g = "g"
+    case mg = "mg"
+    case µg = "µg"
+}
+
+// MARK: - Ingredient
+struct IngredientsResponse: Codable {
+    let text: String
+    let quantity: Double
+    let measure: String?
+    let food: String
+    let weight: Double
+    let foodCategory, foodID: String
+    let image: String
+
+    enum CodingKeys: String, CodingKey {
+        case text, quantity, measure, food, weight, foodCategory
+        case foodID = "foodId"
+        case image
+    }
+}
+
+// MARK: - RecipeLinks
+struct RecipeLinks: Codable {
+    let next: Next
+}
