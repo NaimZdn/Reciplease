d6b6993fe6c6ab0dea1259a4a1e917a21790bf71
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-22 23:12:38 +0200

    feat(network) : create an enum with the API baseUrl

---
+++ Reciplease/Data/Network/APIEndpoints.swift
@@ -0,0 +1,12 @@
+//
+//  APIEndpoints.swift
+//  Reciplease
+//
+//  Created by Zidouni on 20/06/2023.
+//
+
+import Foundation
+
+enum RecipeEndpoint {
+    static let baseUrl = "https://api.edamam.com/api/recipes/v2"
+}
