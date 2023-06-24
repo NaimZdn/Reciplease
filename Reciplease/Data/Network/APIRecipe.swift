0891c47b43a4e7db4f0cb67f7315f5255934e3fd
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-24 23:45:59 +0200

    feat(network) : add the API call

---
+++ Reciplease/Data/Network/APIRecipe.swift
@@ -0,0 +1,74 @@
+//
+//  APIRecipe.swift
+//  Reciplease
+//
+//  Created by Zidouni on 20/06/2023.
+//
+
+
+import Foundation
+import Combine
+import Alamofire
+
+class APIRecipe: ObservableObject {
+    private let baseUrl = RecipeEndpoint.baseUrl
+    private var cancellable: AnyCancellable?
+    private var requestError: Errors? = nil
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    private func getAPIKey(fromFileNamed fileName: String) throws -> String {
+        guard let envPath = Bundle.main.path(forResource: fileName, ofType: "plist"),
+              let env = NSDictionary(contentsOfFile: envPath),
+              let apiKey = env["RECIPE_API_KEY"] as? String else {
+            throw Errors.apiKeyNotFound
+        }
+        return apiKey
+    }
+    
+    private func getAPIId(fromFileNamed fileName: String) throws -> String {
+        guard let envPath = Bundle.main.path(forResource: fileName, ofType: "plist"),
+              let env = NSDictionary(contentsOfFile: envPath),
+              let apiId = env["RECIPE_API_ID"] as? String else {
+            throw Errors.apiKeyNotFound
+        }
+        return apiId
+    }
+    
+    func getAPI(apiEnvFilename: String = "Env", completion: @escaping (Result<Recipe, Errors>) -> Void) {
+        do {
+            let apiKey = try getAPIKey(fromFileNamed: apiEnvFilename)
+            let apiId = try getAPIId(fromFileNamed: apiEnvFilename)
+            
+            let parameters: Parameters = [
+                "app_key": apiKey,
+                "app_id": apiId,
+                "type": "public",
+                "q": "chicken+milk",
+                "field": ["label", "image", "ingredients", "calories", "totalTime", "digest", "yield"]
+                
+            ]
+            
+            let encoding = URLEncoding(arrayEncoding: .noBrackets)
+            
+            AF.request("https://api.edamam.com/api/recipes/v2",  parameters: parameters, encoding: encoding)
+                .validate()
+                .publishDecodable(type: Recipe.self)
+                .compactMap { $0.value }
+                .sink(receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        break
+                    case .failure(let error):
+                        print("Error: \(error)")
+                    }
+                }, receiveValue: { response in
+                    print("dans la requete de base", response)
+                    completion(.success(response))
+                })
+                .store(in: &cancellables)
+        } catch {
+            completion(.failure(error as! Errors))
+        }
+    }
+}
