5ca53508650c01b0e9f0ec19a45fa5de6ec00c81
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-06-25 18:08:54 +0200

    feat(recipe) : add properties to display apiData inside this component

--- Reciplease/Presentation/Recipe/Component/RecipeCard.swift
+++ Reciplease/Presentation/Recipe/Component/RecipeCard.swift
@@ -8,63 +8,71 @@
 import SwiftUI
 
 struct RecipeCard: View {
-    @State private var timerText = "25 min"
-    @State private var tablewarText = "1 ppl"
-    @State private var kcalText = "494 kcal"
+    var title: String
+    var kcalText: String
+    var tablewar: String
+    var timer: String
+    var image: URL
+    var action: () -> Void
     
     var body: some View {
-        HStack{
-            HStack(alignment: .top) {
-                
-                Image("ramen")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 94, height: 94)
-                    .cornerRadius(5)
-                    .clipped()
-                
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Chicken ramen")
-                            .font(.cardValue)
-                            .foregroundColor(.cardTitleIcon)
+        Button {
+            action()
+        } label: {
+            HStack{
+                HStack(alignment: .top) {
+                    ImageView(imageURL: image)
+                        .scaledToFill()
+                        .frame(width: 94, height: 94)
+                        .cornerRadius(5)
+                        .clipped()
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(title)
+                                .multilineTextAlignment(.leading)
+                                .font(.cardValue)
+                                .foregroundColor(.cardTitleIcon)
+                                .fixedSize(horizontal: false, vertical: true)
+                            
+                            Spacer()
+                            
+                            FavoriteButton()
+                        }
+                        .padding(.leading, 7)
                         
+                        if timer != "0" {
+                            HStack {
+                                CardOption(optionText: "\(timer) min", optionIcon: "timer")
+                            }
+                            .padding(.leading, 7)
+                        }
                         Spacer()
                         
-                        FavoriteButton()
-                    }
-                    .padding(.leading, 7)
-                    
-                    HStack {
-                        CardOption(optionText: $timerText, optionIcon: "timer")
-                    }
-                    .padding(.leading, 7)
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Spacer()
-                        CardOption(optionText: $tablewarText, optionIcon: "tablewar")
-                        CardOption(optionText: $kcalText, optionIcon: "kcal")
+                        HStack {
+                            Spacer()
+                            CardOption(optionText: tablewar, optionIcon: "tablewar")
+                            CardOption(optionText: kcalText, optionIcon: "kcal")
+                        }
                     }
                 }
-                .padding(.top, 10)
+                .frame(maxWidth: 314, maxHeight: 94, alignment: .center)
             }
-            .frame(maxWidth: 314, maxHeight: 94)
+            .padding(15)
+            .frame(maxWidth: 340, maxHeight: 120)
+            .background(Color.cardBackground, in: RoundedRectangle(cornerRadius: 10))
+            
         }
-        .padding(15)
-        .frame(maxWidth: 340, maxHeight: 120)
-        .background(Color.cardBackground, in: RoundedRectangle(cornerRadius: 10))
-        
     }
 }
 
 struct RecipeCard_Previews: PreviewProvider {
+    @State static private var imageURL = URL(string: "https://edamam-product-images.s3.amazonaws.com/web-img/873/873d09161f6847aadd22f8c17f3b9e01.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEHsaCXVzLWVhc3QtMSJHMEUCIQDamt%2B50ojX9ESwJLKPaL1IYFKkqFwfNWXZbrYP3xrZ8QIgeRZaXdjPsE6tWurk70z2dkQ5y8aVHRfCQIpq5Um6b7MqwQUI1P%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgwxODcwMTcxNTA5ODYiDNtEOBGBYrmnYpryEyqVBY108K0uBjAKkf5dnddluFcptOUXe5Oz5OZs1mrsy3asRCraDRnN2eysgJCV5Zu1iOpWhjagraIL3IkNkrLfdD2OPLWynPXPjaPkOQcny75%2FyEkukVE5ezHQgcilde7yzCEhLEsuiRfC9qtL7vKz54jvp00XG%2BHwi8GFiOxWF60%2FcjHpxwr%2ByOUkCIjFe64QJmuFM%2FhKe5feduuzXeQ2ptjF%2FwMRMiPAiD4sq4jFRKKi63oAc4ThOVDQ1JzbrMrA3zfdayV9MtpwXubB2PZZeJy2O2LLPpEEz4qUqQSGOgxwx0uJ46MFDsLjLRASlBaA508HfPGRH5kUlvhVCPg3DClR3fqW63COTFR0ZgmDDxnncOlNc99lBT6fPRqU8OGhLq4sJH225KRhD27kbBitwbyx7JeJ3Trp%2FOU1ExS3frxsFFq8LKsgSI3RDzZD8eD6RCKwVHGMMGRLVYq%2BVWf9CMcRYXnfG6bgmKBFCCA4E0HjSexptdhm6zdPGTb0olWKqqJqkaOqmCqxyGsJ1ye5bzOhVFIPJuhKZ4tmZbEkStA2h1E1q3caAq1T0PiauGQkUoou5Umt%2Bz07MJ%2Bs%2FGZEcaVG6PccEJ8LAggTmbiWVuR74lXUv9epd1b6PhFs%2FP4aG47sBgASnUBBgE7op1EK8lv8CoGBvoTxiuixeENSuh5KqtHdrBsR5lrycnMD3qkvjJ7dd4A49NYmqNEOY2hJZScVSVxyCLGmmoLJMRQu1tUpye8vwqqrNbyW0k23vOuSNoObia%2FxsNqe9gjH5HPf8Qb4HdBIGiEIcrHaG%2BnT%2B%2FG9LFfFEK5dXvI8uy9T%2BHb7WyBorJbQ3kI0YDKNCs11Xec2MeN7oyH%2BLCoJwqeimIqCWV2CZXsw8cjQpAY6sQFDnEESrFe%2Bs8ojTk3xRd3h3Is%2FgBSIneanbgWyynHQvbLXyigF2uqDbrenjBh%2FENvgZlzJP5D5zQfiQXM07ZrKunqB3RXWJcI3cxj3XA9doWytFYj2lfmET0IGPChrt06%2BXv8nw3jjlidhY3MOM0E04SCyz3vcsJM37DkhAJJpJQzXDnXjakNu3Dj4HjODUkjf6V%2B9K2XZWrR3aPhLaK60g47lxVnrsXvbjGcM19AebCc%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230622T110702Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFPCDFMDYB%2F20230622%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=9097fc6b98f61131cdf83b1e00a8e4863d59c862780a9d90118043a889bfcc46")!
+    
     static var previews: some View {
         
         VStack {
-            RecipeCard()
+            RecipeCard(title: "Chicken ramen ramen ramen ramen", kcalText: "494 kcal", tablewar: "1 ppl", timer: "0", image: imageURL, action: {})
         }
         .padding(10)
         .background(Color.background)
