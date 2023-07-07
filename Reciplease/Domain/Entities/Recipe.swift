6ec4443c027ce3da551a0a10a8a72e6d247d5bb0
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-07-07 12:57:27 +0200

    feat(recipe) : create a recipe Model

---
+++ Reciplease/Domain/Entities/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  Reciplease
+//
+//  Created by Zidouni on 01/07/2023.
+//
+
+import Foundation
+
+struct Recipe: Hashable {
+    var title: String
+    var stepsLink: String
+    var image: String
+    var kcalText: String
+    var tablewar: String
+    var timer: String
+    var protein: String
+    var fat: String
+    var carb: String
+    var ingredients: [String]
+
+}
